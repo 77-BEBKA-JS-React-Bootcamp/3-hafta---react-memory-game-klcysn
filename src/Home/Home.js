61084dc6ef34e53bd530618cdfb54f1bca566b68
klcysn <klcysn@users.noreply.github.com>
2021-03-04 14:46:10 +0100

    Complete Home page

---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./Home.scss"
+
+
+class Home extends React.Component{
+
+    render(){
+
+        return(
+            <div className="container">
+                <button className="start-button">Start Game</button>
+            </div>
+        )
+    }
+}
+
+export default Home
