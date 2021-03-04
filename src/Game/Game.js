4a80e2871adcc2910a4b1673e7b92b2d5d409e1c
klcysn <klcysn@users.noreply.github.com>
2021-03-04 17:06:27 +0100

    Change assets location

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -2,12 +2,22 @@ import React from "react"
 import "./Game.scss"
 
 
+
 export default class Game extends React.Component{
+    state = {
+        index: [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
+    }
+    componentDidMount(){
+        this.setState({index: this.state.index.sort((a,b)=> 0.5 - Math.random())})
+    }
     render(){
         const {username} =this.props.location.state
         return(
             <div className="game-container">
                 <p className="game-username">{username.toUpperCase()}</p>
+                <div className="cards-container">
+                    {this.state.index.map((i, indx)=><img src="/assets/back.png" alt="" width="170vw" height="auto" />)}
+                </div>
             </div>
         )
     }
